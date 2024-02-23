0bd325079925ff962852a719dde40d08c4570253
Yu Wu <128890731+ywugwu@users.noreply.github.com>
2024-02-23 17:56:19 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='viz-utils',
+    version='0.0.1',
+    url='https://github.com/ywugwu/viz-utils.git',
+    author='ywugui',
+    author_email='yu.wu1@gwu.edu',
+    description='utilties for visualization attention/saliency for NLP models',
+    packages=["viz_utils"],    
+    install_requires=[],
+)
